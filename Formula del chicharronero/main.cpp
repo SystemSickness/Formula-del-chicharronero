2acf86f1a333a3a58162ad62c6fb56fcb50e0628
Jordy <39038507+SystemSickness@users.noreply.github.com>
2018-08-28 10:14:17 -0500

    Update main.cpp

--- Formula del chicharronero/main.cpp
+++ Formula del chicharronero/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <math.h>
+using namespace std;
+int main()
+{
+    float a, b, c, binomio, x1, x2;
+    cout << "\nProblema para la resolucion de ecuaciones cuadraticas por Formula General\n";
+    cout <<"\nIngresa el valor del coheficiente cuadratico (a): \n";
+    cin >> a;
+    cout <<"\n Ingresa el valor del coheficiente lineal (b): \n";
+    cin >> b;
+    cout << "\n Ingresa el valor del termino independiente (c): \n";
+    cin >> c;
+    binomio = (b*b) - (4*a*c);
+    if (binomio < 0)
+    {
+        cout << "\n No es posible calcular el resultado para los valores actuales";
+    
+    }
+    else
+    {
+        x1 = ((b) * (-1) + sqrt (binomio)) / (2*a);
+        x2 = ((b) * (-1) + sqrt (binomio)) / (2*a);
+        cout <<"\n El valor de la raiz X1 es:  " <<x1;
+        cout <<"\n El valor de la raiz X2 es:  " <<x2;
+    }
+    cout <<"\n \n Fin de programa \n \n";
+    system("pause");
 }
